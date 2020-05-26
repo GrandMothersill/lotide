76f15566a6a9b9ca126afa5a1b6915b48387fe9f
Graham Mothersill <graham.mothersill@gmail.com>
2020-05-26 21:27:43 +0000

    created without function

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("These arrays are equal");
+  } else {
+    console.log("These arrays are NOT equal");
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let moddedArray = [];
+  for (let s = 0; s < source.length; s++) {
+    if (!itemsToRemove.includes(source[s])) {
+      moddedArray.push(source[s]);
+    }
+  }
+  return moddedArray;
+};
